d04352419b88dfcb384d69c2f6afcb2715442a9d
Adam Pavlicek <adamhpavlicek@gmail.com>
2019-04-02 22:03:48 -0500

    add html and some js

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const apiKey = "8dPCn2m0srdRnXxgfm1bYK6GN3ODPdaiprU9lLD6";
+const searchURL = 'https://developer.nps.gov/api/v1/parks?parkCode=acad';
+
+// function getPark(){
+
+// }
